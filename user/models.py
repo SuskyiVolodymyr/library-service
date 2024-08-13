f71d1d922cb35fc7bf03fbe4647f1fb1a8d80d88
Viktor Sveda <vinix.ru@gmail.com>
2024-08-13 11:53:37 +0300

    add User end AUTH_USER_MODEL to settings.py

--- user/models.py
+++ user/models.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import (
-    BaseUserManager
+    BaseUserManager,
+    PermissionsMixin
 )
-
+from django.db import models
+from django.utils.translation import gettext as _
 
 
 class CustomUserManager(BaseUserManager):
@@ -57,4 +60,16 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, first_name, last_name, password, **extra_fields)
 
 
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_staff = models.BooleanField(default=False)
 
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return self.email
