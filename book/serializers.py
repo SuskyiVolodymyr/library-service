3744b289be95c7f4ec1e9ee38417e9daf1e25f07
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-13 17:38:45 +0300

    add merging of books when one is same as another after update

--- book/serializers.py
+++ book/serializers.py
@@ -20,3 +20,17 @@ class BookSerializer(serializers.ModelSerializer):
             book.save()
             return book
         return Book.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        book = Book.objects.filter(
+            title=validated_data["title"],
+            author=validated_data["author"],
+            cover=validated_data["cover"],
+        ).exclude(id=instance.id)
+        if book:
+            book = book[0]
+            book.inventory += validated_data["inventory"]
+            book.save()
+            instance.delete()
+            return book
+        return super().update(instance, validated_data)
