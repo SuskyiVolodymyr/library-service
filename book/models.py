035a3ae6e28162fb44265b06f34067cecc1129d0
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-08-13 15:33:31 +0300

    Add save logic to serializers.py in Book app

--- book/models.py
+++ book/models.py
@@ -14,27 +14,5 @@ class Book(models.Model):
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["title", "author", "cover"], name="unique_book_constraint"
-            )
-        ]
-
-    def save(self, *args, **kwargs):
-        existing_book = None
-        if not self.pk:
-            existing_book = Book.objects.filter(
-                title=self.title, author=self.author, cover=self.cover
-            ).first()
-
-        if existing_book:
-            existing_book.inventory += 1
-            existing_book.save()
-        else:
-            if self.inventory is None:
-                self.inventory = 1
-            super(Book, self).save(*args, **kwargs)
-
     def __str__(self):
         return f"{self.title} by {self.author}"
--- book/serializers.py
+++ book/serializers.py
@@ -6,11 +6,17 @@ from book.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = [
-            "id",
-            "title",
-            "author",
-            "cover",
-            "inventory",
-            "daily_fee"
-        ]
+        fields = ["id", "title", "author", "cover", "inventory", "daily_fee"]
+
+    def create(self, validated_data):
+        book = Book.objects.filter(
+            title=validated_data["title"],
+            author=validated_data["author"],
+            cover=validated_data["cover"],
+        )
+        if book:
+            book = book[0]
+            book.inventory += validated_data["inventory"]
+            book.save()
+            return book
+        return Book.objects.create(**validated_data)
