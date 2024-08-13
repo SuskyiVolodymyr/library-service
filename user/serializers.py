23afb8eafcc82365e36b818fd7e61ebeb7523e0a
Viktor Sveda <vinix.ru@gmail.com>
2024-08-13 13:56:30 +0300

    fix Meta in UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -5,8 +5,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "first_name", "last_name", "is_staff")
-        read_only_fields = ("is_staff",)
+        fields = ("email", "password", "first_name", "last_name", "is_staff")
+        read_only_fields = ("id", "is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
