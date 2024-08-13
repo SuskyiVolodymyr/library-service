03169f00e943780f75a7dc259a2a28f50b3435dd
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-08-13 13:32:10 +0300

    Implement save method and checking for uniqueness book/models.py

--- book/models.py
+++ book/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -12,6 +13,29 @@ class Book(models.Model):
     cover = models.CharField(max_length=100, choices=COVER_CHOICES)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "author", "cover"], name="unique_book_constraint"
+            )
+        ]
+
+    def save(self, *args, **kwargs):
+        if (
+            not self.pk
+            and Book.objects.filter(
+                title=self.title, author=self.author, cover=self.cover
+            ).exists()
+        ):
+            raise ValidationError(
+                "A book with this title, author, and cover already exists."
+            )
+
+        if self.inventory is None:
+            self.inventory = 1
+
+        super(Book, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
