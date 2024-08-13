6af533ccd38ac296855805d7ef1e9a404f372299
Viktor Sveda <vinix.ru@gmail.com>
2024-08-13 13:09:12 +0300

    fix User models end add new migration

--- user/models.py
+++ user/models.py
@@ -1,53 +1,28 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import (
-    BaseUserManager,
-    PermissionsMixin
-)
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils.translation import gettext as _
 
 
 class CustomUserManager(BaseUserManager):
-    def _create_user(
-            self,
-            email,
-            first_name,
-            last_name,
-            password=None,
-            **extra_fields
-    ):
+    def _create_user(self, email, first_name, last_name, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
         user = self.model(
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            **extra_fields
+            email=email, first_name=first_name, last_name=last_name, **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(
-            self,
-            email,
-            first_name,
-            last_name,
-            password=None,
-            **extra_fields
-    ):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, first_name, last_name, password, **extra_fields)
 
     def create_superuser(
-            self,
-            email,
-            first_name,
-            last_name,
-            password=None,
-            **extra_fields
+        self, email, first_name, last_name, password=None, **extra_fields
     ):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -65,6 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = CustomUserManager()
 
