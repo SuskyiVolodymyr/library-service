f5fa27fc9be0660c7c593b24f65881bb2092d3cf
Viktor Sveda <vinix.ru@gmail.com>
2024-08-13 11:42:59 +0300

    add create_user end create_superuser in CustomUserManager

--- user/models.py
+++ user/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager
+)
 
-# Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(
+            self,
+            email,
+            first_name,
+            last_name,
+            password=None,
+            **extra_fields
+    ):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(
+            self,
+            email,
+            first_name,
+            last_name,
+            password=None,
+            **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+    def create_superuser(
+            self,
+            email,
+            first_name,
+            last_name,
+            password=None,
+            **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+
+
