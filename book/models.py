709b0722c288b374f45adc6c798046748247b7f0
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-08-13 12:32:13 +0300

    Create model Book in book/models.py

--- book/models.py
+++ book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = [
+        ("HARD", "Hard"),
+        ("SOFT", "Soft"),
+    ]
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=100, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    @property
+    def full_information(self):
+        return (f"{self.title} by {self.author} in {self.cover} cover. "
+                f"Daily fee: {self.daily_fee}. "
+                f"Books you can borrow: {self.inventory}")
