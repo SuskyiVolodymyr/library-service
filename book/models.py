1c30aff5ea867392882d3619893119f5887940ac
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-08-13 14:30:57 +0300

    Add logic in save() in Book model: if book exist -> inventory + 1

--- book/models.py
+++ book/models.py
@@ -22,20 +22,19 @@ class Book(models.Model):
         ]
 
     def save(self, *args, **kwargs):
-        if (
-            not self.pk
-            and Book.objects.filter(
+        existing_book = None
+        if not self.pk:
+            existing_book = Book.objects.filter(
                 title=self.title, author=self.author, cover=self.cover
-            ).exists()
-        ):
-            raise ValidationError(
-                "A book with this title, author, and cover already exists."
-            )
+            ).first()
 
-        if self.inventory is None:
-            self.inventory = 1
-
-        super(Book, self).save(*args, **kwargs)
+        if existing_book:
+            existing_book.inventory += 1
+            existing_book.save()
+        else:
+            if self.inventory is None:
+                self.inventory = 1
+            super(Book, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
