bdf49dad11468736779485a686848b84999ce525
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-14 20:58:04 +0300

    changed borrowing serializers and removed BorrowingReturnSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 from django.db import transaction
 from django.utils import timezone
 from rest_framework import serializers
@@ -9,12 +7,8 @@ from borrowing.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    book = BookReadSerializer(read_only=True)
-    user = serializers.SlugRelatedField(
-        many=False,
-        read_only=True,
-        slug_field="email"
-    )
+    book = BookReadSerializer(read_only=True, many=True)
+    user = serializers.SlugRelatedField(many=False, read_only=True, slug_field="email")
 
     class Meta:
         model = Borrowing
@@ -24,20 +18,15 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
             "book",
-            "user"
+            "user",
         ]
-        read_only_fields = [
-            "id",
-            "borrow_date",
-            "book",
-            "user"
-        ]
+        read_only_fields = ["id", "borrow_date", "book", "user"]
 
 
 class BorrowingCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = ["expected_return_date", "book"]
+        fields = ["id", "expected_return_date", "book"]
 
     def validate(self, attrs):
         borrow_date = timezone.now().date()
@@ -51,47 +40,16 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         with transaction.atomic():
-            book = validated_data.get("book")
-            if book.inventory <= 0:
-                raise serializers.ValidationError("Book is out of stock.")
-            book.inventory -= 1
-            book.save()
+            books = validated_data.pop("book")
+            for book in books:
+                if book.inventory <= 0:
+                    raise serializers.ValidationError("Book is out of stock.")
+                book.inventory -= 1
+                book.save()
 
-            borrowing = Borrowing.objects.create(
-                user=self.context["request"].user,
-                **validated_data
-            )
+                borrowing = Borrowing.objects.create(
+                    user=self.context["request"].user, **validated_data
+                )
+            borrowing.book.add(*books)
 
             return borrowing
-
-
-class BorrowingReturnSerializer(serializers.ModelSerializer):
-    actual_return_date = serializers.DateField()
-
-    class Meta:
-        model = Borrowing
-        fields = ["actual_return_date"]
-
-    def validate_actual_return_date(self, value):
-        if value > date.today():
-            raise serializers.ValidationError(
-                "The actual return date cannot be in the future."
-            )
-        return value
-
-    def validate(self, data):
-        borrowing = self.instance
-
-        if borrowing.actual_return_date is not None:
-            raise serializers.ValidationError(
-                "This book has already been returned."
-            )
-
-        return data
-
-    def update(self, instance, validated_data):
-        instance.actual_return_date = validated_data["actual_return_date"]
-        instance.book.inventory += 1
-        instance.book.save()
-        instance.save()
-        return instance
