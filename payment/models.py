035eea9486e13e8b172e54b6803b7c3946ddf6c6
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-14 20:55:11 +0300

    add status "Canceled" and changed unique condition

--- payment/models.py
+++ payment/models.py
@@ -9,6 +9,7 @@ class Payment(models.Model):
     class StatusEnum(models.TextChoices):
         pending = "1", "Pending"
         paid = "2", "Paid"
+        canceled = "3", "Canceled"
 
     class TypeEnum(models.TextChoices):
         payment = "1", "Payment"
@@ -16,7 +17,10 @@ class Payment(models.Model):
 
     status = EnumField(StatusEnum)
     payment_type = EnumField(TypeEnum)
-    borrowing = models.ForeignKey(Borrowing, unique=True, on_delete=models.CASCADE)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
     session_url = models.URLField()
     session_id = models.CharField(max_length=100)
     money_to_pay = models.DecimalField(decimal_places=2, max_digits=10)
+
+    class Meta:
+        unique_together = ("borrowing", "payment_type")
