47b889a19617c0dabad70ebbfce95f96d2dbe14b
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-14 12:52:16 +0300

    add url for payment

--- payment/urls.py
+++ payment/urls.py
@@ -1,1 +1,19 @@
+from django.urls import path
+
+from payment.views import (
+    CreateCheckoutSessionView,
+    PaymentSuccessView,
+    PaymentCancelView,
+)
+
 app_name = "payment"
+
+urlpatterns = [
+    path(
+        "create-checkout-session/",
+        CreateCheckoutSessionView.as_view(),
+        name="create-checkout-session",
+    ),
+    path("success/", PaymentSuccessView.as_view(), name="payment-success"),
+    path("cancel/", PaymentCancelView.as_view(), name="payment-cancel"),
+]
