4d71007c008d224938c3cbaa177f0a12447dcfac
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-14 20:57:08 +0300

    refactored borrowing models

--- borrowing/models.py
+++ borrowing/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db import models
 
 from book.models import Book
+
 from borrowing.telegram_bot import send_message
 
 
@@ -10,15 +11,9 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
-    book = models.ForeignKey(
-        Book,
-        on_delete=models.CASCADE,
-        related_name="borrowings"
-    )
+    book = models.ManyToManyField(Book, related_name="borrowings")
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="borrowings"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
     )
 
     def __str__(self) -> str:
@@ -28,12 +23,13 @@ class Borrowing(models.Model):
         is_new = self.pk is None
         super().save(*args, **kwargs)
         if is_new:
-            message = (f"New borrowing created:\n"
-                       f"User: {self.user.email}\n"
-                       f"Book: {self.book.title} by {self.book.author}\n"
-                       f"Borrow Date: {self.borrow_date}\n"
-                       f"Expected Return Date: {self.expected_return_date}"
-                       )
+            message = (
+                f"New borrowing created:\n"
+                f"User: {self.user.email}\n"
+                f"Books: "
+                f"Borrow Date: {self.borrow_date}\n"
+                f"Expected Return Date: {self.expected_return_date}"
+            )
             async_to_sync(send_message)(message)
 
     @property
