15635b245c093adc55b1474cb3f16918cbce43b9
Viktor Sveda <vinix.ru@gmail.com>
2024-08-15 13:12:18 +0300

    fix doc in book view

--- book/views.py
+++ book/views.py
@@ -6,13 +6,11 @@ from book.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
+    """List all books. If the user is an admin, all books are listed.
+    If the user is staff, all books are listed.
+    If the user is not admin or staff, only books belonging to the user are listed.
+    """
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
-
-    def list(self, request, *args, **kwargs):
-        """List all books. If the user is an admin, all books are listed.
-        If the user is staff, all books are listed.
-        If the user is not admin or staff, only books belonging to the user are listed.
-        """
-        return super().list(request, *args, **kwargs)
