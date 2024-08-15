f9df642333c3deca6829fc72eb131b698a80a3b9
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-08-15 11:47:05 +0300

    change quoters & add CELERY_BEAT_SCHEDULE

--- library_service/settings.py
+++ library_service/settings.py
@@ -164,7 +164,14 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_API_KEY")
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "UTC"
+
+CELERY_BEAT_SCHEDULE = {
+    "check_overdue_borrowings": {
+        "task": "borrowing.tasks.check_overdue_borrowings",
+        "schedule": timedelta(days=1),
+    },
+}
