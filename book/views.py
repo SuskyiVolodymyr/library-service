1661b1a641749844d99de48d1c5d3506eacb5e87
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-15 14:37:48 +0300

    add pagination for books

--- book/views.py
+++ book/views.py
@@ -1,11 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from book.models import Book
 from book.permissions import IsAdminOrReadOnly
 from book.serializers import BookSerializer
 
 
+class BookPagination(PageNumberPagination):
+    page_size = 5
+    max_page_size = 100
+
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
+    pagination_class = BookPagination
