28e10a098ac8fb4c05763a6bdc26b5a7bf0afc54
Viktor Sveda <vinix.ru@gmail.com>
2024-08-15 12:39:11 +0300

    add doc to views book

--- book/views.py
+++ book/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import viewsets
 
 from book.models import Book
@@ -9,3 +10,10 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        """List all books. If the user is an admin, all books are listed.
+        If the user is staff, all books are listed.
+        If the user is not admin or staff, only books belonging to the user are listed.
+        """
+        return super().list(request, *args, **kwargs)
