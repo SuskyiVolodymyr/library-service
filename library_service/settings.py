fa238153308ff5213b16bd6bf37098b3a9947632
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-08-15 11:29:55 +0300

    add celery

--- library_service/settings.py
+++ library_service/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "book",
     "borrowing",
     "payment",
+    "django_celery_beat"
 ]
 
 MIDDLEWARE = [
@@ -160,3 +161,10 @@ SIMPLE_JWT = {
 }
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_API_KEY")
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
